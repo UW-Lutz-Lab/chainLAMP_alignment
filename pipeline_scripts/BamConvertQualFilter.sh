87cf87fdab5918e34f2e37368e20cf4e3faaf90d
mcgrupp3 <jrupp1@uw.edu>
2024-06-10 17:01:01 -0700

    fixed bam convert syntax

--- pipeline_scripts/BamConvertQualFilter.sh
+++ pipeline_scripts/BamConvertQualFilter.sh
@@ -7,11 +7,13 @@ while [[ "$#" -gt 0 ]]; do
         --reads) reads="$2"; shift ;;
         --output) output="$2"; shift ;;
         --quality_filter) quality_filter="$2"; shift;;
+        --minlength) minlength="$2"; shift;;
+        --maxlength) maxlength="$2"; shift;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-samtools bam2fq "$reads" | chopper -q "$quality_filter" > "$output"
+samtools bam2fq "$reads" | chopper --quality "$quality_filter" --minlength "$minlength"  --maxlength "$maxlength" > "$output"
 
 
